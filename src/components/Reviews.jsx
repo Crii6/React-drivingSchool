91ade1ba0997b94750e633ff99124784b3e4bfdf
Crii6 <lecardonnelgm@gmail.com>
2023-08-21 22:51:36 +0200

    modif review

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,14 +1,38 @@
-
+import { GiTrafficLightsGreen } from "react-icons/gi";
+import { reviews } from "../constants";
 
 const Reviews = () => {
 
    
     return (
         <div className="w-[95%] m-auto pt-20">
-            <div className="bg-green-500 w-[200px] h-[200px] m-auto rounded-2xl rotate-45">
-
-                <h1>HELLO</h1>
+            <div className="text-center uppercase font-semibold text-3xl">
+                <h2>Reviews</h2>
+                <p className="flex justify-center text-primary borderLR text-xl"><GiTrafficLightsGreen className="borderLR"/></p>
             </div>
+                    {reviews.map(review => {
+                        const {nom, Icon, date, coms } = review
+
+                        return (
+                            <div className="bg-gray-200/50 border-green-300 border w-[250px]  m-auto rounded-2xl mt-20">
+                                <div className="w-[95%] m-auto p-4 text-center">
+                                    <div className="mb-2">
+                                        <h3 className="font-semibold text-xl border-b border-black w-[70%] m-auto">{nom}</h3>
+                                    </div>
+                                    <div className="flex justify-center">
+                                        <Icon /><Icon /><Icon /><Icon /><Icon />
+                                    </div>
+                                    <div className="">
+                                        <p className="italic">Publié le {date}</p>
+                                    </div>
+                                    <div className="mt-10">
+                                        <p className="font-semibold">"{coms}"</p>
+                                    </div>
+
+                             </div>
+                         </div>
+                        )
+                    })}
         </div>
     );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -109,7 +109,7 @@ export const permis = [
         title: "BSR",
         Icon: GiScooter,
     },
-]
+];
 
 export const reviews = [
     {
@@ -122,50 +122,43 @@ export const reviews = [
     {
         Icon: AiTwotoneStar,
         id : "coms 2",
-        nom : "Lizzie Ruiz",
-        date : "20/07/2023",
-        coms : ""
-    },
-    {
-        Icon: AiTwotoneStar,
-        id : "coms 3",
         nom : "Ketia",
         date : "25/07/2023",
         coms : "J’ai commencé la conduite  avec Dorothée qui a été une monitrice très patiente et qui a eu confiance en moi malgré mes difficultés avec la conduite au début et ai terminé avec Ève le changement de monitrice ne m’a pas du tout impacté. C’est une monitrice très bienveillante à l’écoute et patiente c’était un plaisir d’apprendre avec vous 2 !! je recommande fortement cette auto école"
     },
     {
         Icon: AiTwotoneStar,
-        id : "coms 4",
+        id : "coms 3",
         nom : "Jean-Baptiste Pot",
         date : "09/08/2023",
         coms : "Excellente auto école ! Je viens d’obtenir mon permis avec 31 points sur 31. La formation est très qualitative, Ève nous forme bien et nous donne les bons conseils pour le permis. La secrétaire est aussi au top, elle nous rappelle nos heures de conduite la veille par sms pour que l’on n’oublie pas, simple et efficace. Ce sont les petites choses qui font la différence ! Je recommande !"
     },
     {
         Icon: AiTwotoneStar,
-        id : "coms 5",
+        id : "coms 4",
         nom : "Oceane Dubois",
         date : "15/06/2023",
         coms : "Superbe auto école je recommande à 100%. J’ai commencé à apprendre avec Dorothee puis Eve, je n’ai pas du tout ressenti le changement de propriétaire, Eve est attentive, agréable et professionnelle, lors des leçons de conduite, elle va toujours chercher le meilleur de vous même. Marie gère très bien l’emploi du temps et fait en sorte d’arranger les élèves au maximum.Une bonne équipe merci à vous.☺️"
     },
     {
         Icon: AiTwotoneStar,
-        id : "coms 6",
+        id : "coms 5",
         nom : "Iris Deleserre",
         date : "18/06/2023",
         coms : "Excellente auto-école avec une très bonne prise en charge ! Merci à la secrétaire ! Une monitrice à l'écoute, bienveillante et patiente pour un bon apprentissage en conduite. Je recommande vivement :)"
     },
     {
         Icon: AiTwotoneStar,
-        id : "coms 7",
+        id : "coms 6",
         nom : "Tatiana Dellinger",
         date : "28/04/2023",
         coms : "Je recommande cette auto-école! Dorothée puis Eve m’ont aidé à prendre confiance en moi pour la conduite, elles sont bienveillantes, à l’écoute et très rassurantes. J’ai été accompagné jusqu’à l’examen. Marie sera toujours à l’écoute et là pour vous informer des cours à l’avance, ou d’autres choses à préparer pour le permis de conduire. Les membres de l’auto école sont excellents, et je recommande cette auto école sans aucun doute!"
     },
     {
         Icon: AiTwotoneStar,
-        id : "coms 8",
+        id : "coms 7",
         nom : "Frederique Thiolat",
         date : "30/05/2023",
         coms : "Je recommande cette auto-école à tout le monde !!!!! Dorothée a passé la main à Eve qui est une excellente monitrice qui sait mettre à l’aise ses élèves. Elle est patiente et attentive au profil de chacun de ses élèves pour les mettre pleinement en confiance pour les faire conduire en toute sérénité. Sans oublier Marie, la secrétaire qui fait tout pour arranger le planning de chacun et répondre à leur besoin. Un grand merci à vous trois pour m’avoir épaulées pour le passage de cet examen. En résumé, n’hésitez pas et foncez vous y inscrire les yeux fermés !!!!!"
     },
-]
+];
