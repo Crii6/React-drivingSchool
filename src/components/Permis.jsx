f7c3a88b0e7205ea0b1f917445d2dd465f120116
Crii6 <lecardonnelgm@gmail.com>
2023-08-22 23:27:41 +0200

    reviews modif

--- src/components/Permis.jsx
+++ src/components/Permis.jsx
@@ -3,20 +3,20 @@ import { permis } from "../constants";
 
 function Permis() {
   return (
-    <div className="py-16 bg-gray-200">
+    <div className="py-16 bg-gray-gradient">
         <div className="w-[90%] text-center max-w-7xl m-auto">
             <h2 className="text-3xl font-semibold uppercase">Categorie Permis</h2>
             <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, deleniti.</p>
             
             <p className="flex justify-center text-primary borderLR text-xl"><GiTrafficLightsGreen className="borderLR"/></p>
         </div>
-        <div className="flex flex-col justify-center items-center mt-10 md:flex-row md:flex-wrap md:justify-around max-w-7xl md:m-auto md:mt-12">
+        <div className="flex flex-col justify-center items-center mt-10 md:flex-row md:flex-wrap md:justify-around max-w-7xl md:m-auto md:mt-12 ">
             {permis.map( permi => {
                 const {title, Icon } = permi
 
                 return (
 
-                <div className="octagon my-5 overflow-hidden">
+                <div className="octagon my-5 overflow-hidden ">
                     <div className="content">
                         <Icon  className="text-3xl text-primary colorHover"/>
                         <h3 className="text-2xl uppercase font-semibold">{title}</h3>
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,25 +1,38 @@
 import { GiTrafficLightsGreen } from "react-icons/gi";
 import { reviews } from "../constants";
+import { Swiper, SwiperSlide} from 'swiper/react';
+import { EffectCards } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
 
 const Reviews = () => {
 
    
     return (
-        <div className="w-[95%] m-auto pt-20">
+        <div className="w-[100%] m-auto pt-20">
             <div className="text-center uppercase font-semibold text-3xl">
-                <h2>Reviews</h2>
+                <h2 className="mb-5">Reviews</h2>
                 <p className="flex justify-center text-primary borderLR text-xl"><GiTrafficLightsGreen className="borderLR"/></p>
             </div>
+                            <Swiper 
+                             effect={'cards'}
+                             grabCursor={true}
+                             modules={[EffectCards]}
+                             className="mySwiper "
+                            >
                     {reviews.map(review => {
                         const {nom, Icon, date, coms } = review
 
                         return (
-                            <div className="bg-gray-200/50 border-green-300 border w-[250px]  m-auto rounded-2xl mt-20">
+                            <SwiperSlide>
+                                    
+                            <div className="bg-gray-100 border border-gray-400 w-[250px] h-[380px] m-auto rounded-2xl mt-16 shadow-xl">
                                 <div className="w-[95%] m-auto p-4 text-center">
                                     <div className="mb-2">
-                                        <h3 className="font-semibold text-xl border-b border-black w-[70%] m-auto">{nom}</h3>
+                                        <h3 className="font-semibold text-xl border-b border-black w-[70%] m-auto text-primary">{nom}</h3>
                                     </div>
-                                    <div className="flex justify-center">
+                                    <div className="flex justify-center text-amber-300">
                                         <Icon /><Icon /><Icon /><Icon /><Icon />
                                     </div>
                                     <div className="">
@@ -27,12 +40,15 @@ const Reviews = () => {
                                     </div>
                                     <div className="mt-10">
                                         <p className="font-semibold">"{coms}"</p>
+                                
                                     </div>
 
                              </div>
                          </div>
+                            </SwiperSlide>
                         )
                     })}
+                        </Swiper>
         </div>
     );
 };
